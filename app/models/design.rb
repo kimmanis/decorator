15a4609a2c144679df54b6f995c2c46234246a8c
kimmanis <kimmanis@gmail.com>
2013-07-23 20:29:08 -0700

    image size large

--- app/models/design.rb
+++ app/models/design.rb
@@ -2,7 +2,7 @@ class Design < ActiveRecord::Base
   belongs_to :user
   attr_accessible :title, :user_id, :image
   has_attached_file :image, 
-  :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+  :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "800x800>" }, 
   :storage => :s3,
  	:s3_credentials => "#{Rails.root}/config/s3_credentials.yml",
 	:path => "/:id/:style/:filename",
